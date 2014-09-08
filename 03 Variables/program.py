5780e1c00a7dbcad25d3713834abc82a9926a69a
Nina Rillera <immortalcooki3@outlook.com>
2014-09-08 13:08:04 +1000

    Added Text Files

---
+++ 03 Variables/program.py
@@ -0,0 +1,6 @@
+a = raw_input("Enter the first number")
+a = int(a)
+b = raw_input("Enter the second number")
+b = int(b)
+answer = a + b
+print "%d + %d = %d" % (a, b, answer)
---
+++ 03 Variables/program2.py
@@ -0,0 +1,3 @@
+car = raw_input("What was the make of your first car?")
+pet = raw_input("What was the name of your first pet's name?")
+print "Your stage name is %s %s" %(car, pet)
